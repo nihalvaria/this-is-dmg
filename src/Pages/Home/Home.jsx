d6e071b20b2f9feafb810da0772e1598cc341b00
ROG401IU\nihal <nihal@tagglabs.in>
2020-10-04 23:28:13 +0530

    Added nav to home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import './Home.scss'
+import Navbar from '../../Containers/Navbar/Navbar'
 
 const Home = () => {
-    
+
 	return (
 		<div className="Home">
-
+            <Navbar />
         </div>
 	)
 }
