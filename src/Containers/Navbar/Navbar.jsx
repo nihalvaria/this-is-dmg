e4568e25fb6d3103056b633d3e4d826fb9210cdc
ROG401IU\nihal <nihal@tagglabs.in>
2020-10-05 02:16:26 +0530

    Navbar responsive

--- src/Containers/Navbar/Navbar.jsx
+++ src/Containers/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import OrdersImage from "Assets/Images/document-text-sharp.png"
 import OptionsImage from "Assets/Images/options-sharp.png"
 import AccountImage from "Assets/Images/person-sharp.png"
 import CartImage from "Assets/Images/cart-sharp-nav.png"
+import SearchImage from "Assets/Images/search.png"
 import NotificationImage from "Assets/Images/active.png"
 
 
@@ -33,7 +34,11 @@ const Navbar = () => {
             <div className="options-container flex">
                 <img src={ OptionsImage } alt="options-img" />
             </div>
-            <div className="flex">
+            <div className="search-container flex">
+                <input type="text" placeholder="Search"/>
+                <img src={ SearchImage } alt="search-img" />
+            </div>
+            <div className="nav-side-items-container flex">
                 {
                     navSideItems.map( item => (
                         <div key={ item.name } className="nav-side-item flex">
