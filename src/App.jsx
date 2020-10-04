6dfe17aa8f581f2cbd500217eb068d10427eedb6
ROG401IU\nihal <nihal@tagglabs.in>
2020-10-04 19:25:05 +0530

    Home for * route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import Home from './Pages/Home/Home'
 
 const App = () => {
 	return (
 		<>
 			<Switch>
 				<Route exact path={ '*' } >
-					{/* { Home Component } */}
+					<Home />
 				</Route>
 			</Switch>
 		</>
