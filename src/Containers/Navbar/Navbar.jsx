8baa26bf9aaf6b5ae44d0bdf552b0d35453b81db
ROG401IU\nihal <nihal@tagglabs.in>
2020-10-05 00:17:10 +0530

    Options icon responsive

--- src/Containers/Navbar/Navbar.jsx
+++ src/Containers/Navbar/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import "./Navbar.scss"
+import OptionsImage from "Assets/Images/options-sharp.png"
+
 
 const Navbar = () => {
     
 	return (
-		<nav className="Navbar">
-
+		<nav className="Navbar flex">
+            <div className="options-container flex">
+                <img src={ OptionsImage } alt="options-img" />
+            </div>
         </nav>
 	)
 }
