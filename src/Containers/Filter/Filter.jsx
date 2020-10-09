ba2ca44f94a070d1a6ef59da105af3a6413eceb1
ROG401IU\nihal <nihal@tagglabs.in>
2020-10-10 01:29:40 +0530

    Added filter-con sizing

--- src/Containers/Filter/Filter.jsx
+++ src/Containers/Filter/Filter.jsx
@@ -1,12 +1,17 @@
 import SectionHeader from 'Components/Filter/SectionHeader/SectionHeader'
+import SelectedFilters from 'Components/Filter/SelectedFilters/SelectedFilters'
 import React from 'react'
 import './Filter.scss'
 
 const Filter = () => {
     
     return (
-        <div className="Filter">
+        <div className="Filter flex">
             <SectionHeader />
+
+            <div className="filters-container flex">
+                <SelectedFilters />
+            </div>
         </div>
     )
 }
