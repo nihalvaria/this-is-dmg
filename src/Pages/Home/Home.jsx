4ef062dca2c291dbeaf532a7d4a03ef3e8add175
ROG401IU\nihal <nihal@tagglabs.in>
2020-10-10 00:33:47 +0530

    Added Filter Container in Home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Home.scss'
 import Navbar from 'Containers/Navbar/Navbar'
+import Filter from 'Containers/Filter/Filter'
 
 const Home = () => {
 
 	return (
 		<div className="Home">
             <Navbar />
+			<Filter />
         </div>
 	)
 }
