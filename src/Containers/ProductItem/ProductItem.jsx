ad466d4e6bce49594209a68f05df22a22114d441
ROG401IU\nihal <nihal@tagglabs.in>
2020-10-10 19:59:33 +0530

    Product Item with no styles

--- src/Containers/ProductItem/ProductItem.jsx
+++ src/Containers/ProductItem/ProductItem.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import "./ProductItem.scss"
+import ItemCTABtn from 'Components/ProductItem/ItemCTABtn/ItemCTABtn'
 
-const ProductItem = () => {
+const ProductItem = props => {
 
     return (
-        <div className="ProductItem">
-            
+        <div className={ `ProductItem ${ props.data.featured ? 'featured' : 'basic' }` }>
+            <ItemCTABtn state={ props.data.stock === 0 ? 'notify' : 'add' } featured={ props.data.featured } />
         </div>
     )
 }
