958befc416334d92af5122c3446b63c219155c56
ROG401IU\nihal <nihal@tagglabs.in>
2020-10-10 19:59:46 +0530

    Product List to map

---
+++ src/Data/ProductList.js
@@ -0,0 +1,188 @@
+import productImage from 'Assets/Images/tyre.png'
+import badge from 'Assets/Images/badge.png'
+import aprovedBadge from 'Assets/Images/appr.png'
+
+export const productList = {
+    "products": [
+        {
+            "id": 1,
+            "name": "Continental - Contiecocontact 5",
+            "size": "205/55 R16 H",
+            "vehicle": "SUV",
+            "productImage": productImage,
+            "badge": badge,
+            "approvedBy": aprovedBadge,
+            "currency": "₪",
+            "price": "340",
+            "stock": 10,
+            "stockUpdateTime": "1 week",
+            "featured": "Premium Bestseller",
+            "discount": {
+                "minOrder": 8,
+                "percentage": 15
+            }
+        },
+        {
+            "id": 2,
+            "name": "Continental - Contiecocontact 5",
+            "size": "205/55 R16 H",
+            "vehicle": "SUV",
+            "productImage": productImage,
+            "badge": badge,
+            "approvedBy": aprovedBadge,
+            "currency": "₪",
+            "price": "340",
+            "stock": 10,
+            "stockUpdateTime": "1 week",
+            "featured": "Premium Bestseller",
+            "discount": {
+                "minOrder": 8,
+                "percentage": 15
+            }
+        },
+        {
+            "id": 3,
+            "name": "Continental PremiumContact™ 6",
+            "size": "195/55 R15 85V",
+            "vehicle": "SUV",
+            "productImage": "",
+            "badge": "",
+            "approvedBy": aprovedBadge,
+            "currency": "₪",
+            "price": "340",
+            "stock": 0,
+            "stockUpdateTime": "1 week",
+            "featured": null,
+            "discount": {
+                "minOrder": 8,
+                "percentage": 15
+            }
+        },
+        {
+            "id": 4,
+            "name": "Continental PremiumContact™ 6",
+            "size": "195/55 R15 85V",
+            "vehicle": "SUV",
+            "productImage": "",
+            "badge": "",
+            "approvedBy": aprovedBadge,
+            "currency": "₪",
+            "price": "340",
+            "stock": 8,
+            "stockUpdateTime": "1 week",
+            "featured": null,
+            "discount": {
+                "minOrder": 8,
+                "percentage": 15
+            }
+        },
+        {
+            "id": 5,
+            "name": "Continental PremiumContact™ 6",
+            "size": "195/55 R15 85V",
+            "vehicle": "SUV",
+            "productImage": "",
+            "badge": "",
+            "approvedBy": aprovedBadge,
+            "currency": "₪",
+            "price": "340",
+            "stock": 0,
+            "stockUpdateTime": "1 week",
+            "featured": null,
+            "discount": {
+                "minOrder": 8,
+                "percentage": 15
+            }
+        },
+        {
+            "id": 6,
+            "name": "Continental PremiumContact™ 6",
+            "size": "195/55 R15 85V",
+            "vehicle": "SUV",
+            "productImage": "",
+            "badge": "",
+            "approvedBy": aprovedBadge,
+            "currency": "₪",
+            "price": "340",
+            "stock": 100,
+            "stockUpdateTime": "1 week",
+            "featured": null,
+            "discount": {
+                "minOrder": 8,
+                "percentage": 15
+            }
+        },
+        {
+            "id": 7,
+            "name": "Continental PremiumContact™ 6",
+            "size": "195/55 R15 85V",
+            "vehicle": "SUV",
+            "productImage": "",
+            "badge": "",
+            "approvedBy": aprovedBadge,
+            "currency": "₪",
+            "price": "340",
+            "stock": 100,
+            "stockUpdateTime": "1 week",
+            "featured": null,
+            "discount": {
+                "minOrder": 8,
+                "percentage": 15
+            }
+        },
+        {
+            "id": 8,
+            "name": "Continental PremiumContact™ 6",
+            "size": "195/55 R15 85V",
+            "vehicle": "SUV",
+            "productImage": "",
+            "badge": "",
+            "approvedBy": aprovedBadge,
+            "currency": "₪",
+            "price": "340",
+            "stock": 100,
+            "stockUpdateTime": "1 week",
+            "featured": null,
+            "discount": {
+                "minOrder": 8,
+                "percentage": 15
+            }
+        },
+        {
+            "id": 9,
+            "name": "Continental PremiumContact™ 6",
+            "size": "195/55 R15 85V",
+            "vehicle": "SUV",
+            "productImage": "",
+            "badge": "",
+            "approvedBy": aprovedBadge,
+            "currency": "₪",
+            "price": "340",
+            "stock": 100,
+            "stockUpdateTime": "1 week",
+            "featured": null,
+            "discount": {
+                "minOrder": 8,
+                "percentage": 15
+            }
+        },
+        {
+            "id": 10,
+            "name": "Continental PremiumContact™ 6",
+            "size": "195/55 R15 85V",
+            "vehicle": "SUV",
+            "productImage": "",
+            "badge": "",
+            "approvedBy": aprovedBadge,
+            "currency": "₪",
+            "price": "340",
+            "stock": 8,
+            "stockUpdateTime": "1 week",
+            "featured": null,
+            "discount": {
+                "minOrder": 8,
+                "percentage": 15
+            }
+        }
+    ]
+}
