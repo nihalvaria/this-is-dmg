922d3af14a4df934b96f8604159d4eca7555e996
ROG401IU\nihal <nihal@tagglabs.in>
2020-10-12 01:11:08 +0530

    Mapping Products

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,9 @@ import './Home.scss'
 import Navbar from 'Containers/Navbar/Navbar'
 import Filter from 'Containers/Filter/Filter'
 import FullWidthButton from 'Components/Common/FullWidthButton/FullWidthButton'
+import ProductItem from 'Containers/ProductItem/ProductItem'
+
+import { productList } from 'Data/ProductList'
 
 const Home = () => {
 
@@ -10,6 +13,24 @@ const Home = () => {
 		<div className="Home flex">
             <Navbar />
 			<Filter />
+			<div className="featured-list flex">
+				{
+					productList[ 'products' ].map( item => {
+						if( item.featured ) {
+							return <ProductItem key={ item.id } data={ item } />
+						}
+					} )
+				}
+			</div>
+			<div className="product-list flex">
+				{
+					productList[ 'products' ].map( item => {
+						if( !item.featured ) {
+							return <ProductItem key={ item.id } data={ item } />
+						}
+					} )
+				}
+			</div>
 			<FullWidthButton value={ 'Load More Items' } handleClick={ () => console.log( 'load more items' ) } />
         </div>
 	)
